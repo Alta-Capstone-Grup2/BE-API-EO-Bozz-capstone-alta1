389f778f5d147f2882e11e6b670bc23b9f0d0d6e
mfaishal882 <mfaishal882@gmail.com>
2022-12-19 13:45:59 +0700

    init, user, login

---
+++ factory/factory.go
@@ -0,0 +1,25 @@
+package factory
+
+import (
+	authDelivery "capstone-alta1/features/auth/delivery"
+	authRepo "capstone-alta1/features/auth/repository"
+	authService "capstone-alta1/features/auth/service"
+
+	userDelivery "capstone-alta1/features/user/delivery"
+	userRepo "capstone-alta1/features/user/repository"
+	userService "capstone-alta1/features/user/service"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func InitFactory(e *echo.Echo, db *gorm.DB) {
+	userRepoFactory := userRepo.New(db)
+	// userRepoFactory := userRepo.NewRaw(db)
+	userServiceFactory := userService.New(userRepoFactory)
+	userDelivery.New(userServiceFactory, e)
+
+	authRepoFactory := authRepo.New(db)
+	authServiceFactory := authService.New(authRepoFactory)
+	authDelivery.New(authServiceFactory, e)
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"capstone-alta1/config"
+	"capstone-alta1/factory"
+	"capstone-alta1/middlewares"
+	"capstone-alta1/utils/database/mysql"
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	cfg := config.GetConfig()
+	db := mysql.InitDB(cfg)
+	// db := posgresql.InitDB(cfg)
+
+	e := echo.New()
+
+	factory.InitFactory(e, db)
+
+	// middleware
+	middlewares.LogMiddlewares(e)
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", cfg.SERVER_PORT)))
+}
