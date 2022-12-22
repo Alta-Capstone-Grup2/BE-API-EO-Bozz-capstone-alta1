5d72fc5e1a2b0dabd6ad20e5a20b48fdd60e0ec7
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2022-12-22 09:04:07 +0700

    add open api new

--- config/config.go
+++ config/config.go
@@ -43,7 +43,6 @@ func initConfig() *AppConfig {
 		}
 	}
 
-	// SECRET = os.Getenv("SECRET")
 	cnvServerPort, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
 	if err != nil {
 		log.Fatal("Cannot parse Server Port variable")
@@ -61,19 +60,6 @@ func initConfig() *AppConfig {
 	}
 	defaultConfig.DB_PORT = uint(cnvDBPort)
 	defaultConfig.JWT_SECRET = os.Getenv("JWT_SECRET")
-
-	// log.Println(` Env List :
-	//  - server port : `, defaultConfig.SERVER_PORT,
-	// 	` \n - db name : `, defaultConfig.DB_NAME,
-	// 	` \n - db username : `, defaultConfig.DB_USERNAME,
-	// 	` \n - db password : `, defaultConfig.DB_PASSWORD,
-	// 	` \n - db host : `, defaultConfig.DB_HOST,
-	// 	` \n - db port : `, defaultConfig.DB_PORT,
-	// 	` \n - jwt secret : `, defaultConfig.JWT_SECRET,
-	// 	` \n - AWS_REGION : `, os.Getenv("JWT_SECRET"),
-	// 	` \n - AWS_BUCKET_NAME : `, os.Getenv("AWS_BUCKET_NAME"),
-	// 	` \n - ACCESS_KEY_IAM : `, os.Getenv("ACCESS_KEY_IAM"),
-	// 	` \n - SECRET_KEY_IAM : `, os.Getenv("SECRET_KEY_IAM"))
 
 	return &defaultConfig
 }
