0c0f101a3561c32119e9342481226b683b329ed8
mfaishal882 <mfaishal882@gmail.com>
2023-01-02 11:06:39 +0700

    add coreapi req

--- utils/thirdparty/midtrans.go
+++ utils/thirdparty/midtrans.go
@@ -39,3 +39,22 @@ func CheckMidtrans(orderId string) *coreapi.TransactionStatusResponse {
 	res, _ := c.CheckTransaction(orderId)
 	return res
 }
+
+func OrderMidtransCore(orderId string, grossAmmount int64, paymentType midtrans.Bank) *coreapi.ChargeResponse {
+	midtrans.ServerKey = os.Getenv("MIDTRANS_SERVER")
+	midtrans.ClientKey = os.Getenv("MIDTRANS_CLIENT")
+	midtrans.Environment = midtrans.Sandbox
+	c := coreapi.Client{}
+	c.New(os.Getenv("MIDTRANS_SERVER"), midtrans.Sandbox)
+
+	req := &coreapi.ChargeReq{
+		PaymentType: coreapi.CoreapiPaymentType(paymentType),
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  orderId,
+			GrossAmt: grossAmmount,
+		},
+	}
+
+	chargeRes, _ := coreapi.ChargeTransaction(req)
+	return chargeRes
+}
